dbeda8b4fb7cf5c68d48d4d61551c63440836501
Flatiron School <flatironschool@Flatirons-MacBook-Air-2.local>
2017-09-20 10:58:21 -0400

    migrated db and set up authorization methods in application controller

--- db/migrate/20170920142412_create_recipes.rb
+++ db/migrate/20170920142412_create_recipes.rb
@@ -1,6 +1,13 @@
 class CreateRecipes < ActiveRecord::Migration[5.1]
   def change
     create_table :recipes do |t|
+    	t.string :name
+    	t.string :description
+    	t.string :instruction
+    	t.float :og
+    	t.float :fg
+    	t.float :abv
+    	t.string :image
     end
   end
 end
--- db/migrate/20170920142434_create_recipe_ingredients.rb
+++ db/migrate/20170920142434_create_recipe_ingredients.rb
@@ -3,7 +3,7 @@ class CreateRecipeIngredients < ActiveRecord::Migration[5.1]
     create_table :recipe_ingredients do |t|
     	t.integer :recipe_id
     	t.integer :ingredient_id
-    	t.integer :amount
+    	t.float :amount
     	t.string :unit
     end
   end
