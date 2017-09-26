2e68512b2e43d1f7704b9955fe8724542df703e5
Luke Pierotti <lpierotti2@gmail.com>
2017-09-26 11:08:16 -0400

    following works MVP done

--- app/controllers/api/v1/follows_controller.rb
+++ app/controllers/api/v1/follows_controller.rb
@@ -1,16 +1,25 @@
 class Api::V1::FollowsController < ApplicationController
 
 	def create
+		byebug
 		user = current_user
 		follow = Follow.create(follower_id: user.id, followee_id: params[:id])
-		render json: {followee: follow.followee_id}
+		followee = User.find_by(id: params[:id])
+		render json: {followee: follow}
 	end
 
 	def index
 		user = User.find_by(id: params[:id])
 		followers = Follow.where("followee_id = ?", user.id).pluck(:follower_id)
+		followers = followers.map do |follower| 
+			User.find_by(id: follower)
+		end
 		followees = Follow.where("follower_id = ?", user.id).pluck(:followee_id)
-		render json: {follower_ids: followers, followee_ids: followees}
+		followees = followees.map do |followee|
+			User.find_by(id: followee)
+		end
+		byebug
+		render json: {followers: followers, followees: followees}
 	end
 
 end
