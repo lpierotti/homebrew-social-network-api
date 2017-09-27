ada8cc4e9fa454a7f36ca4fc5471be92c7d66e6e
Luke Pierotti <lpierotti2@gmail.com>
2017-09-27 09:12:16 -0400

    added groups and formatted render of review to display properly

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -2,7 +2,9 @@ class Api::V1::ReviewsController < ApplicationController
 
 	def create
 		review = Review.create(user_id: current_user.id, recipe_id: params[:recipeId], rating: params[:review][:rating], text: params[:review][:text])
-		render json: {review: review}
+		formatted_review = review.attributes
+		formatted_review[:author] = review.user
+		render json: {review: formatted_review}
 	end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170926175654) do
+ActiveRecord::Schema.define(version: 20170926214414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,9 @@ ActiveRecord::Schema.define(version: 20170926175654) do
   create_table "follows", force: :cascade do |t|
     t.integer "follower_id"
     t.integer "followee_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
   end
 
   create_table "ingredients", force: :cascade do |t|
@@ -48,6 +51,9 @@ ActiveRecord::Schema.define(version: 20170926175654) do
     t.integer "rating"
     t.integer "recipe_id"
     t.integer "user_id"
+  end
+
+  create_table "user_groups", force: :cascade do |t|
   end
 
   create_table "user_recipes", force: :cascade do |t|
